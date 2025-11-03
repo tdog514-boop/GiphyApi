47eff1b048cf3258a603207346914e5594fdaac3
trey.costner@erskine.edu <treycostner55@gmail.com>
2025-11-02 23:43:02 -0500

    Add Giphy API endpoint and const

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("script.js loaded");
+function endpoint(tag) {
+  return `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${tag}&rating=g`;
+}
+
+const apiKey = "hqsdJh6cVQX1jOnm9IxE4I1fYeqLUNDv";
+
+const gifContainer = document.querySelector("#gif-container");
+const fetchButton = document.querySelector("#fetch-gif-btn");
+const searchInput = document.querySelector("#search-input");
